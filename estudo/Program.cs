e7dca0cbc3edce92a5f0d731e2828cb6b7215999
Caio Martins do Nascimento <martinscaio29@gmail.com>
2023-10-15 18:43:59 -0300

    f

--- estudo/Program.cs
+++ estudo/Program.cs
@@ -163,11 +163,47 @@ namespace livrocsharp
             }
             nomes_funcionarios.RemoveAt(0);
             WriteLine(nomes_funcionarios[0]);
+
+
+            WriteLine("==========================");
+            WriteLine("Orientação a objetos:");
         }
 
 
     }
 
+    // Declarando uma classe genérica
+    public class ListaGenerica<T>
+    {
+        public void Adicionar(T input) { }
     }
+    class TestListaGenerica
+    {
+        private class ExampleClass { }
+        static void Testa()
+        {
+            // Declarando uma lista do tipo inteiro
+            ListaGenerica<int> lista1 = new ListaGenerica<int>();
+           
+
+            lista1.Adicionar(1);
+            // Declarando uma lista do tipo string
+            ListaGenerica<string> lista2 = new ListaGenerica<string>();
+            lista2.Adicionar("");
+            /// Declarando uma lista do tipo de uma classe
+             ListaGenerica<ExampleClass> lista3 = new ListaGenerica<ExampleClass>();
+            lista3.Adicionar(new ExampleClass());
+        }
+    }
+
+   
+}
+
+
+
+
+
+
+
 
 
